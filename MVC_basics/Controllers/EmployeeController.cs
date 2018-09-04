1ceec3d2622ae167f626b22e8226a3d619a3da63
Nandakumar Kadam <nandakumar.kadam@slksoft.com>
2018-09-04 10:55:33 +0530

    trying again

---
+++ MVC_basics/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_basics.Models;
+
+namespace MVC_basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        //public ActionResult Details()
+        //{
+        //    Employee employee = new Employee()
+        //    {
+        //        EmployeeId = 101,
+        //        Name = "John",
+        //        Gender = "Male",
+        //        City = "New York"
+        //    };
+
+        //    return View(employee);
+        //}
+
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employees = employeeContext.Employees.ToList();
+
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+}
